30c104d54e6d2af989cb1b171ea4e6f15e6a9a3f
sundev79 <45767598+sunmediaproject@users.noreply.github.com>
2019-07-03 16:40:40 +0200

    change method createGameDir

--- src/main/java/fr/theshark34/openlauncherlib/minecraft/util/GameDirGenerator.java
+++ src/main/java/fr/theshark34/openlauncherlib/minecraft/util/GameDirGenerator.java
@@ -43,15 +43,19 @@ public class GameDirGenerator
      *
      * @return The generated game directory
      */
-    public static File createGameDir(String serverName)
-    {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("win"))
-            return new File(System.getProperty("user.home") + "\\AppData\\Roaming\\." + serverName);
-        else if (os.contains("mac"))
-            return new File(System.getProperty("user.home") + "/Library/Application Support/" + serverName);
-        else
-            return new File(System.getProperty("user.home") + "/." + serverName);
-    }
+//    public static File createGameDir(String serverName)
+//    {
+//        String os = System.getProperty("os.name").toLowerCase();
+//        if (os.contains("win"))
+//            return new File(System.getProperty("user.home") + "\\AppData\\Roaming\\." + serverName);
+//        else if (os.contains("mac"))
+//            return new File(System.getProperty("user.home") + "/Library/Application Support/" + serverName);
+//        else
+//            return new File(System.getProperty("user.home") + "/." + serverName);
+//    }
+	// compatible any OS !
+	public static File createGameDir(String serverName) {
+		return new File(System.getenv("HOME") + "/" + serverName);
+	}
 
 }
